8700b5da078c4f4b15d394549dfa3f214292f058
Dmitrii Kotov <dk@tutunak.com>
2022-12-21 17:23:57 +0300

    added peers parser

--- cosmos_collector.py
+++ cosmos_collector.py
@@ -6,7 +6,7 @@ from prometheus_client import start_http_server, Gauge
 
 
 class CustomExporter:
-    def __init__(self, endpoint="http://localhost:1317", port=9877) -> None:
+    def __init__(self, endpoint="http://localhost:26657", port=9877) -> None:
         self.endpoint = endpoint
         self.port = port
         self.metric_dict = {}
@@ -18,7 +18,7 @@ class CustomExporter:
         time_diff = current_time - block_time
         metric.set(time_diff.seconds)
 
-    def set_metrics(self, block_header):
+    def set_metrics(self, block_header, peers):
         if self.metric_dict.get("time_diff") is None:
             self.metric_dict["time_diff"] = Gauge("time_diff", "time diff")
         self.__time_metric(self.metric_dict["time_diff"], block_header["time"])
@@ -27,11 +27,19 @@ class CustomExporter:
             self.metric_dict["block_height"] = Gauge("block_height", "block_height")
         self.metric_dict["block_height"].set(int(block_header["height"]))
 
+        if self.metric_dict.get("count_peers") is None:
+            self.metric_dict["count_peers"] = Gauge("count_peers", "count_peers")
+        self.metric_dict["count_peers"].set(peers)
+
     def set_value(self, metric_name, block_hash, block_height, value):
         self.metric_dict[metric_name].labels(block_hash, block_height).set(value)
 
+    def get_peers(self):
+        response = requests.get(f"{self.endpoint}/net_info")
+        return response.json()["result"]["n_peers"]
+
     def get_block(self):
-        response = requests.get(f"{self.endpoint}/blocks/latest")
+        response = requests.get(f"{self.endpoint}/block")
         return response.json()
 
     def __last_block_hash(self):
@@ -41,7 +49,8 @@ class CustomExporter:
         start_http_server(self.port)
         while True:
             block = self.get_block()
-            self.set_metrics(block["block"]["header"])
+            peers = self.get_peers()
+            self.set_metrics(block["result"]["block"]["header"], peers)
             time.sleep(1)
 
 
